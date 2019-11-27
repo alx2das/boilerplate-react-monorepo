ba0f3dc095460e7834efd824c69bc1f107bb62d7
Alexandr Buylov <alx2das@gmail.com>
2019-11-27 15:15:37 +0500

    init

---
+++ apps/main/src/main.tsx
@@ -0,0 +1,9 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app/app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
